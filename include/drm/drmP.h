6bab9eb64e542e9d2baf8e54dfaa71680c180321
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-12-16 11:21:06 +0100

    drm: rename dev->count_lock to dev->buf_lock
    Since really that's all it protects - legacy horror stories in
    drm_bufs.c. Since I don't want to waste any more time on this I didn't
    bother to actually look at what it protects in there, but it's at
    least contained now.
    
    v2: Move the spurious hunk to the right patch (Thierry).
    
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 2177a2182f3f375f64d9938dd884895c3872c380)
    
    Signed-off-by: Dheeraj Jamwal <dheerajx.s.jamwal@intel.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1070,7 +1070,6 @@ struct drm_device {
 
 	/** \name Locks */
 	/*@{ */
-	spinlock_t count_lock;		/**< For inuse, drm_device::open_count, drm_device::buf_use */
 	struct mutex struct_mutex;	/**< For others */
 	struct mutex master_mutex;      /**< For drm_minor::master and drm_file::is_master */
 	/*@} */
@@ -1078,6 +1077,7 @@ struct drm_device {
 	/** \name Usage Counters */
 	/*@{ */
 	int open_count;			/**< Outstanding files open, protected by drm_global_mutex. */
+	spinlock_t buf_lock;		/**< For drm_device::buf_use and a few other things. */
 	int buf_use;			/**< Buffers in use -- cannot alloc */
 	atomic_t buf_alloc;		/**< Buffer allocation in progress */
 	/*@} */
