35d652c6c42a104dfdb9a839d40b938d45e47ecc
Ben Widawsky <benjamin.widawsky@intel.com>
2014-05-06 22:21:36 -0700

    drm/i915: Make aliasing a 2nd class VM
    There is a good debate to be had about how best to fit the aliasing
    PPGTT into the code. However, as it stands right now, getting aliasing
    PPGTT bindings is a hack, and done through implicit arguments. To make
    this absolutely clear, WARN and return an error if a driver writer tries
    to do something they shouldn't.
    
    I have no issue with an eventual revert of this patch. It makes sense
    for what we have today.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 6e7186af3b815adda710ee8b4a6f1402776661c0)
    
    Signed-off-by: Dheeraj Jamwal <dheerajx.s.jamwal@intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -3993,8 +3993,12 @@ i915_gem_object_pin(struct drm_i915_gem_object *obj,
 		    uint32_t alignment,
 		    uint64_t flags)
 {
+	struct drm_i915_private *dev_priv = obj->base.dev->dev_private;
 	struct i915_vma *vma;
 	int ret;
+
+	if (WARN_ON(vm == &dev_priv->mm.aliasing_ppgtt->base))
+		return -ENODEV;
 
 	if (WARN_ON(flags & (PIN_GLOBAL | PIN_MAPPABLE) && !i915_is_ggtt(vm)))
 		return -EINVAL;
