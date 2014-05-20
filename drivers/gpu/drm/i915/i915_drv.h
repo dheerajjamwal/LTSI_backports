2f66dbe00ee20965b153580c49ec06c838068d0c
Chris Wilson <chris@chris-wilson.co.uk>
2014-05-20 08:28:43 +0100

    drm/i915: Implement an oom-notifier for last resort shrinking
    Before the process killer is invoked, oom-notifiers are executed for one
    last try at recovering pages. We can hook into this callback to be sure
    that everything that can be is purged from our page lists, and to give a
    summary of how much memory is still pinned by the GPU in the case of an
    oom. This should be really valuable for debugging OOM issues.
    
    Note that the last-ditch effort call to shrink_all we've previously
    called from our normal shrinker when we could free as much as the vm
    demaned is moved into the oom notifier. Since the shrinker accounting
    races against bind/unbind operations we might have called shrink_all
    prematurely, which this approach with an oom notifier avoids.
    
    References: https://bugs.freedesktop.org/show_bug.cgi?id=72742
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Tested-by: lu hua <huax.lu@intel.com>
    [danvet: Bikeshed logical | into || and pimp commit message.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 2cfcd32a929b21c3cf77256dd8b858c076803ccc)
    
    Signed-off-by: Dheeraj Jamwal <dheerajx.s.jamwal@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1045,6 +1045,7 @@ struct i915_gem_mm {
 	/** PPGTT used for aliasing the PPGTT with the GTT */
 	struct i915_hw_ppgtt *aliasing_ppgtt;
 
+	struct notifier_block oom_notifier;
 	struct shrinker shrinker;
 	bool shrinker_no_lock_stealing;
 
